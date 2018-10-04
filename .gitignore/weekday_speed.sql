06a4666009fef379435712c3347419f61b575883
akxuan <hei.bobby@gmail.com>
2018-10-04 17:55:55 -0500

    Update weekday_speed.sql

--- .gitignore/weekday_speed.sql
+++ .gitignore/weekday_speed.sql
@@ -1,5 +1,13 @@
+select roadwayid, pointcode,
+ concat (107,
+case
+WHEN SUBSTR(roadwayid,4,1)  = '-' 
+               THEN 'N'
+               ELSE 'P' 
+       END , pointcode)as tmc,
 
-select roadwayid, pointcode, roaddescription, speedcapped,
+
+ roaddescription, speedcapped,
 basetimestamp, 
 CASE WHEN EXTRACT(hour FROM basetimestamp)  between 6 and 9    THEN 1
      WHEN EXTRACT(hour FROM basetimestamp)    between 10 and 14  THEN 2
@@ -8,9 +16,11 @@ CASE WHEN EXTRACT(hour FROM basetimestamp)  between 6 and 9    THEN 1
 end as peak_hr,
 CASE WHEN EXTRACT(day FROM basetimestamp)  =1   THEN 1
 WHEN EXTRACT(day FROM basetimestamp)  =2   THEN 2
+WHEN EXTRACT(day FROM basetimestamp)  =3  THEN 3
+WHEN EXTRACT(day FROM basetimestamp)  =4   THEN 4
 WHEN EXTRACT(day FROM basetimestamp)  =28   THEN 5
 WHEN EXTRACT(day FROM basetimestamp)  =29   THEN 6
 WHEN EXTRACT(day FROM basetimestamp)  =30   THEN 7
  ELSE 0 
  end as day_week
-from   public.here_roadway_flow  where basetimestamp > '2018-9-28 0:0:0' and basetimestamp < '2018-10-2 0:0:0' 
+from   public.here_roadway_flow  where basetimestamp > '2018-9-28 0:0:0' and basetimestamp < '2018-10-4 0:0:0'  limit 100
