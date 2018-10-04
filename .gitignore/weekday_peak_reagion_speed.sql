46d85725b7ec88b0c66acb3a26ace8b2fec674dd
akxuan <hei.bobby@gmail.com>
2018-10-04 18:25:51 -0500

    Update weekday_peak_reagion_speed.sql

--- .gitignore/weekday_peak_reagion_speed.sql
+++ .gitignore/weekday_peak_reagion_speed.sql
@@ -1,15 +1,14 @@
+
 select region,region_id,peak_hr,day_week,avg(speedcapped) from
 ( select * from 
 (select roadwayid, pointcode,
- concat (107,
+concat (107,
 case
 WHEN SUBSTR(roadwayid,4,1)  = '-' 
                THEN 'N'
                ELSE 'P' 
        END , pointcode)as tmc,
-
-
- roaddescription, speedcapped,
+roaddescription, speedcapped,
 basetimestamp, 
 CASE WHEN EXTRACT(hour FROM basetimestamp)  between 6 and 9    THEN 1
      WHEN EXTRACT(hour FROM basetimestamp)    between 10 and 14  THEN 2
@@ -23,10 +22,9 @@ WHEN EXTRACT(day FROM basetimestamp)  =4   THEN 4
 WHEN EXTRACT(day FROM basetimestamp)  =28   THEN 5
 WHEN EXTRACT(day FROM basetimestamp)  =29   THEN 6
 WHEN EXTRACT(day FROM basetimestamp)  =30   THEN 7
- ELSE 0 
- end as day_week
+ELSE 0 
+end as day_week
 from   public.here_roadway_flow  where basetimestamp > '2018-9-28 0:0:0' and basetimestamp < '2018-10-4 0:0:0' ) tt1
-
 left join 
 (select region_id,region,tmc from public.here_tmc_link_region_temp ) tmc_r using (tmc)) tttt
 group by region,region_id,peak_hr,day_week
