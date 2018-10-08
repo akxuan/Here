4127630d92652068f703c87a66bd5f2c400468ca
akxuan <hei.bobby@gmail.com>
2018-10-08 18:15:31 -0500

    Update weekday_peak_reagion_speed.sql

--- .gitignore/weekday_peak_reagion_speed.sql
+++ .gitignore/weekday_peak_reagion_speed.sql
@@ -1,6 +1,7 @@
+select tmc,zonal_id, weekday,avg(speedcapped) from
+(  
 
-select region,region_id,peak_hr,day_week,avg(speedcapped) from
-( select * from 
+select  tmc,zonal_id, speedcapped,  peak_hr, weekday   from 
 (select roadwayid, pointcode,
 concat (107,
 case
@@ -14,17 +15,17 @@ CASE WHEN EXTRACT(hour FROM basetimestamp)  between 6 and 9    THEN 1
      WHEN EXTRACT(hour FROM basetimestamp)    between 10 and 14  THEN 2
 	WHEN EXTRACT(hour FROM basetimestamp)  between 15 and 18    THEN 3
              ELSE 0 
-end as peak_hr,
-CASE WHEN EXTRACT(day FROM basetimestamp)  =1   THEN 1
-WHEN EXTRACT(day FROM basetimestamp)  =2   THEN 2
-WHEN EXTRACT(day FROM basetimestamp)  =3  THEN 3
-WHEN EXTRACT(day FROM basetimestamp)  =4   THEN 4
-WHEN EXTRACT(day FROM basetimestamp)  =28   THEN 5
-WHEN EXTRACT(day FROM basetimestamp)  =29   THEN 6
-WHEN EXTRACT(day FROM basetimestamp)  =30   THEN 7
-ELSE 0 
-end as day_week
-from   public.here_roadway_flow  where basetimestamp > '2018-9-28 0:0:0' and basetimestamp < '2018-10-4 0:0:0' ) tt1
-left join 
-(select region_id,region,tmc from public.here_tmc_link_region_temp ) tmc_r using (tmc)) tttt
-group by region,region_id,peak_hr,day_week
+end as peak_hr
+,to_char(basetimestamp,'DY')  weekday
+from   public.here_roadway_flow  where basetimestamp > '2018-9-7 0:0:0' and basetimestamp < '2018-10-7 0:0:0'  and EXTRACT(hour FROM basetimestamp) >6 and   EXTRACT(hour FROM basetimestamp) <18
+
+limit 100
+
+) tt1
+
+
+inner join 
+(select tmc, zonal_id,contracc  from public.here_xml_region_zone_8_temp where contracc = 'N'     ) tmc_r using (tmc)
+
+) tttt
+group by tmc,zonal_id, speedcapped, weekday
