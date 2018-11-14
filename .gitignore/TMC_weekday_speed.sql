40632fe4a25d913c50a84e0220047f9eca7b1984
akxuan <hei.bobby@gmail.com>
2018-11-14 12:24:35 -0600

    Update TMC_weekday_speed.sql

--- .gitignore/TMC_weekday_speed.sql
+++ .gitignore/TMC_weekday_speed.sql
@@ -14,13 +14,14 @@ CASE WHEN EXTRACT(hour FROM basetimestamp)  between 6 and 9    THEN 1
 	WHEN EXTRACT(hour FROM basetimestamp)  between 15 and 18    THEN 3
              ELSE 0 
 end as peak_hr,
-CASE WHEN EXTRACT(day FROM basetimestamp)  =1   THEN 1
-WHEN EXTRACT(day FROM basetimestamp)  =2   THEN 2
-WHEN EXTRACT(day FROM basetimestamp)  =3  THEN 3
-WHEN EXTRACT(day FROM basetimestamp)  =4   THEN 4
+CASE WHEN EXTRACT(day FROM basetimestamp)  =24   THEN 1
+WHEN EXTRACT(day FROM basetimestamp)  =25   THEN 2
+WHEN EXTRACT(day FROM basetimestamp)  =26  THEN 3
+WHEN EXTRACT(day FROM basetimestamp)  =27   THEN 4
 WHEN EXTRACT(day FROM basetimestamp)  =28   THEN 5
-WHEN EXTRACT(day FROM basetimestamp)  =29   THEN 6
-WHEN EXTRACT(day FROM basetimestamp)  =30   THEN 7
+--WHEN EXTRACT(day FROM basetimestamp)  =29   THEN 6
+--WHEN EXTRACT(day FROM basetimestamp)  =30   THEN 7
  ELSE 0 
  end as day_week
-from   public.here_roadway_flow  where basetimestamp > '2018-9-28 0:0:0' and basetimestamp < '2018-10-4 0:0:0'  
+ 
+from   public.here_roadway_flow  where basetimestamp > '2018-9-24 0:0:0' and basetimestamp < '2018-9-28 0:0:0'  
